03666582113cc11c1ae1763e343c754d6cda6f7b
Paris <pariswolfe30@gmail.com>
2024-11-14 22:51:13 -0500

    Edited index, app and styles

--- app.js
+++ app.js
@@ -1,33 +1,22 @@
-window.onload = function() {
-    var searchButton = document.getElementById('searchBtn');
-    var searchField = document.getElementById('search');
-    var resultDiv = document.getElementById("heroLst");  // Updated to target the correct element for displaying results
-    var request;
+window.onload= function(){
+var button = document.getElementById("btn");
+var result= document.getElementById("result");
+var searchValue=document.getElementById("search");
 
-    var form = document.getElementsByTagName('form')[0];
-    form.addEventListener('submit', function(event) {
-        event.preventDefault();  // Prevent form submission on Enter key
-    });
-
-    searchButton.addEventListener("click", function() {
-        request = new XMLHttpRequest();
-
-        var query = searchField.value.trim();
-        var url = "http://localhost/info2180-lab4/superheroes.php?query=" + encodeURIComponent(query);
-        console.log(url);
-
-        request.open('GET', url, true);
-        request.send();
-
-        request.onreadystatechange = function() {
-            if (request.readyState === XMLHttpRequest.DONE) {
-                if (request.status === 200) {
-                    var response = request.responseText;
-                    resultDiv.innerHTML = response;  // Display response in result div
-                } else {
-                    alert("Error, Unable to generate list!");
-                }
-            }
-        };
-    });
-};
+button.addEventListener("click",function(){
+    Req = new XMLHttpRequest();
+    var url="http://localhost/info2180-lab4/superheroes.php?query="+searchValue.value+"";
+    Req.open('GET',url);
+    Req.send();
+    Req.onreadystatechange=respond;
+});
+function respond() {
+    if (Req.readyState === XMLHttpRequest.DONE) {
+      if (Req.status === 200) {
+        result.innerHTML=Req.responseText;
+      } else {
+        alert('Error');
+      }
+    }
+  }
+}
